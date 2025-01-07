205b0a3229edf81fb2c9107c1f92ad24e429acd0
dev-yeon <110480577+dev-yeon@users.noreply.github.com>
2025-01-07 21:22:35 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,8 +1,18 @@
 def solution(array, commands):
     answer = []
-    for command in commands:
-        i, j, k = command 
-        sub_array = array[i - 1:j]  # i부터 j까지의 배열을 슬라이싱
-        sub_array.sort()  # 정렬
-        answer.append(sub_array[k - 1])  # k번째 값을 결과에 추가
-    return answer
+    for command in commands: 
+        i,j,k = command
+        sub_array = array[i - 1:j] 
+        sorted_array = selection_sort(sub_array)  # 선택 정렬 호출
+        answer.append(sorted_array[k-1])
+    return answer 
+
+def selection_sort(array):
+    n = len(array)
+    for i in range(n-1):
+        min_index = i
+        for j in range(i +1, n):
+            if array[j] < array[min_index]:
+                min_index = j 
+        array[i], array[min_index] = array[min_index], array[i]
+    return array 
