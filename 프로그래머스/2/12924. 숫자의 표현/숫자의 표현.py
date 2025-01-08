661ca3c062ac292c8b0541acb9474492cd2737f1
dev-yeon <110480577+dev-yeon@users.noreply.github.com>
2025-01-08 14:44:55 +0900

    [level 2] Title: 숫자의 표현, Time: 4.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        current_sum = 0
+        j = i
+        while current_sum < n:
+            current_sum += j
+            j += 1
+        if current_sum == n:
+            answer += 1
+    return answer
