26d54ef090bce6ceef84511fc5fc5035c2d6354b
dev-yeon <110480577+dev-yeon@users.noreply.github.com>
2025-02-04 20:58:36 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -4,15 +4,18 @@ def solution(array, commands):
     for command in commands: 
         i,j,k = command
         sub_array = array[i - 1:j] 
-        sorted_array = bubble_sort(sub_array)  # 선택 정렬 호출
+        sorted_array = insertion_sort(sub_array)  # 선택 정렬 호출
         answer.append(sorted_array[k-1])
     return answer 
 
-def bubble_sort(array):
-    # 버블정렬을 사용해서 리스트를 정렬하기 
+def insertion_sort(array):
+    # 삽입정렬을 사용해서 리스트를 정렬하기 
     n = len(array)
-    for i in range(n):
-        for j in range(n - i - 1): 
-            if array[j] > array[j + 1] :  # 오름 차순 정렬 
-                array[j], array[j+1] =array[j+1], array[j]
+    for i in range(1,n):
+        key = array[i]
+        j = i - 1
+        while j >= 0 and array[j] > key : # key보다 큰 값들을 한칸씩 뒤로 이동
+            array[ j + 1 ] = array[j]
+            j -= 1
+        array[j + 1] = key 
     return array 
